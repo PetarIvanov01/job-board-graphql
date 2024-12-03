19d603a138df7ebcf9559d0465d005276fca058a
PetarIvanov01 <123456789petar45@gmail.com>
2024-12-03 19:00:48 +0200

    query and display job

--- src/lib/graphql/queries.js
+++ src/lib/graphql/queries.js
@@ -21,3 +21,23 @@ export async function getJobs() {
   const data = await client.request(document);
   return data.jobs;
 }
+export async function getJob(jobId) {
+  const document = gql`
+    query getJob($jobId: ID!) {
+      job(id: $jobId) {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const variables = { jobId };
+
+  const data = await client.request(document, variables);
+  return data.job;
+}
