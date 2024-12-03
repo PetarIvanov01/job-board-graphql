e1457cc599ba3f7173233e28fd83d397c09dbf63
PetarIvanov01 <123456789petar45@gmail.com>
2024-12-03 20:57:39 +0200

    add custom errors

--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,31 @@
+import { GraphQLError } from "graphql";
 import { getCompany } from "./db/companies.js";
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
-    company: (_, args) => getCompany(args.id),
-    job: (_root, args) => getJob(args.id),
+    company: async (_, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw new GraphQLError("No Company found with id " + id, {
+          extensions: {
+            code: "NOT_FOUND",
+          },
+        });
+      }
+      return company;
+    },
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw new GraphQLError("No Job found with id " + id, {
+          extensions: {
+            code: "NOT_FOUND",
+          },
+        });
+      }
+    },
+
     jobs: () => getJobs(),
   },
 
