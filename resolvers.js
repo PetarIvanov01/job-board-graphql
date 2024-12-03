1707310f2848df454d7e50c6bd1a3d9e87bd7317
PetarIvanov01 <123456789petar45@gmail.com>
2024-12-03 17:21:40 +0200

    define job schema and resolvers

---
+++ resolvers.js
@@ -0,0 +1,23 @@
+export const resolvers = {
+  Query: {
+    jobs: () => {
+      return [
+        {
+          id: "test-id-1",
+          title: "The Title 1",
+          description: "The description",
+        },
+        {
+          id: "test-id-2",
+          title: "The Title 2",
+          description: "The description",
+        },
+        {
+          id: "test-id-3",
+          title: "The Title 3",
+          description: "The description",
+        },
+      ];
+    },
+  },
+};
