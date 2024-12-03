a47bb355970704aa5eeb30988cc5e12f5ed177b6
PetarIvanov01 <123456789petar45@gmail.com>
2024-12-03 17:56:48 +0200

    Complete setup grapql

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,4 @@
+import { getCompany } from "./db/companies.js";
 import { getJobs } from "./db/jobs.js";
 
 export const resolvers = {
@@ -6,6 +7,7 @@ export const resolvers = {
   },
 
   Job: {
+    company: async (job) => getCompany(job.companyId),
     date: (job) => job.createdAt.slice(0, "yyyy-mm-dd".length),
   },
 };
