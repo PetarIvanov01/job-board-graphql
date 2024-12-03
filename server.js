d38f5b46213da408af09f006a1dcccc4acc3cd9e
PetarIvanov01 <123456789petar45@gmail.com>
2024-12-03 16:59:52 +0200

    add apollo

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-import cors from 'cors';
-import express from 'express';
-import { authMiddleware, handleLogin } from './auth.js';
+import { ApolloServer } from "@apollo/server";
+
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
 
 const PORT = 9000;
 
+const apolloServer = new ApolloServer({});
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
-app.post('/login', handleLogin);
+app.post("/login", handleLogin);
+
+await apolloServer.start();
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
