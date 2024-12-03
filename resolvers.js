60dca056d150346eba9df6f40d85c10cf5275f9c
PetarIvanov01 <123456789petar45@gmail.com>
2024-12-03 17:36:23 +0200

    add resolver to date

--- resolvers.js
+++ resolvers.js
@@ -1,23 +1,11 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    jobs: () => {
-      return [
-        {
-          id: "test-id-1",
-          title: "The Title 1",
-          description: "The description",
-        },
-        {
-          id: "test-id-2",
-          title: "The Title 2",
-          description: "The description",
-        },
-        {
-          id: "test-id-3",
-          title: "The Title 3",
-          description: "The description",
-        },
-      ];
-    },
+    jobs: () => getJobs(),
+  },
+
+  Job: {
+    date: (job) => job.createdAt.slice(0, "yyyy-mm-dd".length),
   },
 };
